168694f2045e7b57a3013574e7b844d762c91de9
Ranjith Neela <iamranjithneela@gmail.com>
2025-08-11 13:38:14 -0400

    Deploy resume website

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/resume-website/',
+  base: '/portfolio/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
